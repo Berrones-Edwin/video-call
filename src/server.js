36297c7e3f5ae04dc1976800b82da86f7c387dd7
Berrones Edwin <Berrones.Edwin@hotmail.com>
2022-04-23 12:19:46 -0500

    fix: fix add statics files

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(express.static(__dirname + "/public/"));
 
 // Create the twilioClient
 const twilioClient = require('twilio')(
